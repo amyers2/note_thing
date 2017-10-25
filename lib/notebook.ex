fd31c7f6458839228128e321dcffd7512f5625f8
pragdave <dave@pragdave.me>
2017-10-25 16:10:07 -0500

    initial load

---
+++ lib/notebook.ex
@@ -0,0 +1,18 @@
+defmodule Notebook do
+  @moduledoc """
+  Documentation for Notebook.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Notebook.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
